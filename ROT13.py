45fd82a560b3e896f83ddc491cab91f511f9e869
Covax84 <python.momentum@gmail.com>
2019-11-16 17:31:38 +0300

    ROT13 encryption

---
+++ ROT13.py
@@ -0,0 +1,15 @@
+def rot13(message: str):
+    """ Input: string
+        Output: string where every letter replaced with the 13th letter after it, in the alphabet.
+    """
+    alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    encrypted = ''
+    for i in range(len(message)):
+        if message[i] in alphabet_upper:
+            encrypted += (alphabet_upper[(alphabet_upper.find(message[i]) + 13) % 26])
+        elif message[i] in alphabet_lower:
+            encrypted += (alphabet_lower[(alphabet_lower.find(message[i]) + 13) % 26])
+        else:
+            encrypted += message[i]
+    return encrypted
