354e5e203d4deabcb0eb83f4d03fdeb9effb74c1
Covax84 <python.momentum@gmail.com>
2019-11-16 08:31:01 +0300

    function sorting odd numbers in array

---
+++ array_odd_sort.py
@@ -0,0 +1,11 @@
+def sort_array(source_array):
+    """ Input: array of numbers.
+        Output: same array where odd numbers are sorted but even numbers on their places.
+    """
+    for i in range(len(source_array)):
+        if source_array[i] % 2 != 0:
+            for j in range(len(source_array)):
+                if source_array[j] % 2 != 0:
+                    if source_array[j] > source_array[i]:
+                        source_array[j], source_array[i] = source_array[i], source_array[j]
+    return source_array
