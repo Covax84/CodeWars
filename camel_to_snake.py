24c3ede081c9af764bfd66e867231c026b1de560
Covax84 <python.momentum@gmail.com>
2019-11-21 18:44:08 +0300

    CamelCase to snake_case

--- camel_to_snake.py
+++ camel_to_snake.py
@@ -1,4 +1,4 @@
-def to_underscore(string):
+def to_underscore(string: str):
     """ Input: CamelCase string or number
         Output: snake_case string
     """
@@ -12,7 +12,7 @@ def to_underscore1(string: str):
     return ''.join(map(lambda x: ('_' + x.lower()) if x.isupper() else x, str(string))).lstrip('_')
 
 
-def to_underscore2(string):
+def to_underscore2(string: str):
     """ function/method so that it takes CamelCase string and
         returns the string in snake_case notation. Lowercase characters can be numbers.
         If method gets number, it should return string.
@@ -28,9 +28,9 @@ def to_underscore2(string):
     return snake_string.lower().lstrip('_')
 
 
-int(to_underscore('My3CodeIs4TimesBetter'))
-print(to_underscore('ThisIsBeautifulDay'))
-print(to_underscore('TestController'))  # returns test_controller
-print(to_underscore('MoviesAndBooks'))  # returns movies_and_books
-print(to_underscore('App7Test'))  # returns app7_test
-print(to_underscore(1))  # returns "1"
+print(to_underscore('My3CodeIs4TimesBetter'))  # my3_code_is4_times_better
+print(to_underscore('ThisIsBeautifulDay'))  # this_is_beautiful_day
+print(to_underscore('TestController'))  # test_controller
+print(to_underscore('MoviesAndBooks'))  # movies_and_books
+print(to_underscore('App7Test'))  # app7_test
+print(to_underscore(1))  # "1"
