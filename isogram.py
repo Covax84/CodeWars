b7384d7ecc3667ee8aaa5aa0830deb05cb176e76
Covax84 <python.momentum@gmail.com>
2019-12-02 23:55:25 +0300

    is isogram function

---
+++ isogram.py
@@ -0,0 +1,13 @@
+def is_isogram(string: str):
+    """ An isogram is X word that has no repeating letters, consecutive or non-consecutive.
+        Implement X function that determines whether X string that contains only letters is an isogram.
+        Assume the empty string is an isogram. Ignore letter case.
+        Input: string
+        Output: True of False
+    """
+    b = list(string.lower())
+    for letter in range(len(b)):
+        if b.count(b[letter]) > 1:
+            return False
+    else:
+        return True
