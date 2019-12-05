7d6ca9bf39b08fae43647e33414ca35d4235a6db
Covax <57707580+Covax84@users.noreply.github.com>
2019-12-05 19:58:38 +0300

    Input/output annotations. Removed useless comment.

--- move_zeros_to_right.py
+++ move_zeros_to_right.py
@@ -1,4 +1,4 @@
-def move_zeros(array):
+def move_zeros(array: list) -> list:
     """ Input: array
         Output: same array with all the zeros moved to the end.
     """
@@ -13,17 +13,13 @@ def move_zeros(array):
     return array
 
 
-print(move_zeros([9, 0.0, 0, 9, 1, 2, 0, 1, 0, 1, 0.0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
-
-print(move_zeros([0, 0, 0, 0, 0, 0, 0, 'a', 'b', None, 'c', 'd', 1, False, 1, 3, [], 1, 9, {}, 9, 0, 0, 0]))
-# [0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 1, 9, 9, 0, 0, 0]
-# [0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 1, 9, 9, 0, 0, 0]
-# ['a', 'b', None, 'c', 'd', 1, False, 1, 3, [], 1, 9, {}, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-# ['a', 'b', None, 'c', 'd', False, [], {}, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+# print(move_zeros([9, 0.0, 0, 9, 1, 2, 0, 1, 0, 1, 0.0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+# 
+# print(move_zeros([0, 0, 0, 0, 0, 0, 0, 'a', 'b', None, 'c', 'd', 1, False, 1, 3, [], 1, 9, {}, 9, 0, 0, 0]))
+# 
 # print(move_zeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
 # [ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]
-
+# 
 # print(move_zeros([9, 0.0, 0, 9, 1, 2, 0, 1, 0, 1, 0.0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
 # [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 #
@@ -37,15 +33,3 @@ print(move_zeros([0, 0, 0, 0, 0, 0, 0, 'a', 'b', None, 'c', 'd', 1, False, 1, 3,
 # print(move_zeros(["a", "b"]))  # ["a","b"])
 # print(move_zeros(["a"]))  # ["a"])
 # print(move_zeros([0, 0]))  # [0,0]
-
-# def move_zeros(array):
-#     new_array = []
-#     for elem in array:
-#         if type(elem) == float and elem == 0.0:
-#             continue
-#         elif type(elem) == int and elem == 0:
-#             continue
-#         new_array.append(elem)
-#     array = new_array + [0] * (len(array) - (len(new_array)))
-#     return array
-#
