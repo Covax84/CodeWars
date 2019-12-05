a4ae835abc6a5a84e863fa23c0a4c3283f4160ad
Covax <57707580+Covax84@users.noreply.github.com>
2019-12-05 19:55:58 +0300

    Input/output annotations.

--- song_decoder.py
+++ song_decoder.py
@@ -1,13 +1,13 @@
-def song_decoder(song):
-    """ input: string(song) where words separated by several WUBs (dubstep remix).
-        output: string without WUBs with words separated by a single space (original).
+def song_decoder(song: str) -> str:
+    """ Input: string(song) where words separated by several WUBs (dubstep remix).
+        Output: string without WUBs with words separated by a single space (original).
     """
     return song.replace('WUB', ' ').replace('  ', ' ').replace('  ', ' ').strip()
 
 
-def song_decoder2(song):
-    """ input: string(song) where words separated by several WUBs (dubstep remix).
-        output: string without WUBs with words separated by a single space (original).
+def song_decoder2(song: str) -> str:
+    """ Input: string(song) where words separated by several WUBs (dubstep remix).
+        Output: string without WUBs with words separated by a single space (original).
         v.2.0
     """
     return " ".join(song.replace('WUB', ' ').split())
