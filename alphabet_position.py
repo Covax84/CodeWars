1b0f796f681d0e3d322d15d0d385e872578608ff
Covax84 <python.momentum@gmail.com>
2020-01-28 10:14:03 +0300

    In this kata you are required to, given a string, replace every letter with its position in the alphabet.
    If anything in the text isn't a letter, ignore it and don't return it.
    
    "a" = 1, "b" = 2, etc.

--- alphabet_position.py
+++ alphabet_position.py
@@ -1,5 +1,6 @@
 def alphabet_position(text: str) -> str:
     """ Input: text as string
         Output: string with every letter replaced with its position in the alphabet
+                replaced characters separated by one space
     """
     return ' '.join([str(ord(x.lower()) - 96) for x in text if x.isalpha()])
