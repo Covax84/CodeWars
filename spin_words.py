cd0229879d3d6a3250ea9931cd591e365c9bfb7d
Covax84 <python.momentum@gmail.com>
2020-01-30 13:34:18 +0300

    function returns given text where words with length >= 5 reversed

---
+++ spin_words.py
@@ -0,0 +1,6 @@
+def spin_words(sentence):
+    """
+    :param sentence: Text as string
+    :return: Same text where words with length >= 5 reversed
+    """
+    return ' '.join([word[::-1] if len(word) >= 5 else word for word in sentence.split()])
